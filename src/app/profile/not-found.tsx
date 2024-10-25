a9b33cd355b702f3e4657c430a0fb875fb51194c
Mohamed Abdelrahim <morahim888@gmail.com>
2024-10-25 15:57:39 +0100

    Added some sample2 code to all files

--- src/app/profile/not-found.tsx
+++ src/app/profile/not-found.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
-type Props = {}
-
-const not-found = (props: Props) => {
+export default function page() {
   return (
-    <div>not-found</div>
+    <div>page</div>
   )
 }
