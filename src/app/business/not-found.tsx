8b8b65e979ffde0b711fa4edadaaaaf40cf5914e
Mohamed Abdelrahim <morahim888@gmail.com>
2024-10-25 15:40:04 +0100

    Added some sample code to all files

---
+++ src/app/business/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+export default function not-found({}: Props) {
+  return (
+    <div>not-found</div>
+  )
+}
---
+++ src/app/profile/not-found.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const not-found = (props: Props) => {
+  return (
+    <div>not-found</div>
+  )
+}
