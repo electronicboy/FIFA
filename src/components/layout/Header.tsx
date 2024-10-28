5ff436105201579013f0bac52e0a440df1112015
Michelle <michelle@empact.co.za>
2024-10-28 23:04:15 +0000

    Clerk and Header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,34 +1,98 @@
-import { Image, Link } from "@chakra-ui/react";
+"use client";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Center, Image, Link } from "@chakra-ui/react";
+import MobileMenu from "./MobileMenu";
+import {
+  ClerkLoaded,
+  ClerkLoading,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 export default function Header() {
+  const [isOpen, setOpen] = React.useState(false);
+
   return (
-    <header>
-      <div className="NavBar flex">
-        <div className="logo flex-row">
-          <Link href="#">
+    <div className="h-24 flex items-center justify-between">
+      {/* LEFT */}
+      <div className="md:hidden lg:block w-[20%]">
+        <Link href="/" className="font-bold text-xl text-blue-600">
+          🚀 FinditFast
+        </Link>
+      </div>
+      {/* CENTER */}
+      <div className="hidden md:flex w-[50%] text-sm items-center justify-between">
+        {/* LINKS */}
+        <div className="flex gap-6 text-gray-600">
+          <Link href="/" className="flex items-center gap-2">
             <Image
-              src="/FinditFasterLOGObigger.png"
-              alt="Find if Faster Logo"
-              className="logo pl-10"
+              src="/home.png"
+              alt="Homepage"
+              width={6}
+              height={6}
+              className="w-4 h-4"
             />
+            <span>Home</span>
+          </Link>
+          <Link href="/" className="flex items-center gap-2">
+            <Image
+              src="/about.png"
+              alt="About"
+              width={6}
+              height={6}
+              className="w-4 h-4"
+            />
+            <span>About</span>
+          </Link>
+          <Link href="/" className="flex items-center gap-2">
+            <Image
+              src="/business.png"
+              alt="Business"
+              width={6}
+              height={6}
+              className="w-4 h-4"
+            />
+            <span>Business</span>
           </Link>
         </div>
-        <div className="p-8">
-          <ul className="flex justify-between">
-            <li className="about pr-10 justify-between">
-              <Link href="about" className=" justify-between">
-                About
-              </Link>
-            </li>
-            <li className="locations pr-10">
-              <Link href="/business">Businesses</Link>
-            </li>
-            <li className="categories pr-10">
-              <Link href="/categories">Categories</Link>
-            </li>
-          </ul>
+        <div className="hidden xl:flex p-2 bg-slate-100 items-center rounded-xl">
+          <input
+            type="text"
+            placeholder="search..."
+            className="bg-transparent outline-none"
+          />
+          <Image src="/search.png" alt="" width={5} height={5} />
         </div>
       </div>
-    </header>
+      {/* RIGHT */}
+      <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
+        <ClerkLoading>
+          <div className="inline-block h-4 w-4 animate-spin rounded-full border-2 border-gray-500 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white" />
+        </ClerkLoading>
+        <ClerkLoaded>
+          <SignedIn>
+            <div className="cursor-pointer text-cyan-950">
+              <Image src="/people.png" alt="" width={6} height={6} />
+            </div>
+            <div className="cursor-pointer">
+              <Image src="/messages.png" alt="" width={5} height={5} />
+            </div>
+            <div className="cursor-pointer">
+              <Image src="/notifications.png" alt="" width={6} height={6} />
+            </div>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
+            <div className="flex items-center gap-2 text-sm">
+              <Image src="/login.png" alt="" width={6} height={6} />
+              <Link href="/sign-in">Login/Register</Link>
+            </div>
+          </SignedOut>
+        </ClerkLoaded>
+        <MobileMenu />
+      </div>
+    </div>
   );
 }
