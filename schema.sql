cdc5ea3da0ab3328ed7443199bd1fb157097f4ce
Shane Freeder <theboyetronic@gmail.com>
2024-10-29 14:36:52 +0000

    more tweaks

--- schema.sql
+++ schema.sql
@@ -6,25 +6,39 @@ CREATE TABLE IF NOT EXISTS biz_users
 );
 
 
+CREATE TABLE IF NOT EXISTS biz_locations
+(
+    id            INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    location_name TEXT NOT NULL,
+    country       TEXT NOT NULL,
+    UNIQUE (location_name, country)
+);
 
 CREATE TABLE IF NOT EXISTS biz_businesses
 (
-    id          INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
---     name        TEXT NOT NULL,
---     location    TEXT NULL,
---     website     TEXT NULL,
---     phone       TEXT NULL,
---     logo        TEXT NULL,
---     image_thumb TEXT NULL,
---     image_large TEXT NULL,
-    business_id TEXT NULL
+    id             INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name           TEXT                           NOT NULL,
+    location       TEXT                           NULL, -- full address from RAPI
+    website        TEXT                           NULL,
+    phone          TEXT                           NULL,
+    logo           TEXT                           NULL,
+    image_thumb    TEXT                           NULL,
+    image_large    TEXT                           NULL,
+    business_id    TEXT                           NULL, -- From rapidAPI
+    longitude      FLOAT                          NULL,
+    latitude       FLOAT                          NULL,
+    street_address TEXT                           NULL,
+    zipcode        TEXT                           NULL,
+    location       INT REFERENCES biz_locations (id) NOT NULL,
+    --country     TEXT NULL, -- Now inside of City, because, duh
+    hours          JSON                           NULL
 );
 
 CREATE TABLE IF NOT EXISTS biz_reviews
 (
     id       INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     business INT REFERENCES biz_businesses (id),
-    userId INT  NOT NULL REFERENCES biz_users (id),
+    user_id   INT  NOT NULL REFERENCES biz_users (id),
     comment  TEXT NOT NULL,
     review   INT  NOT NULL
 );
