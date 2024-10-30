83f67326dc3b6e762ff0953e3df3bc664b6f1a4f
Michelle <michelle@empact.co.za>
2024-10-30 13:40:12 +0000

    business page and footer dynamic update

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,43 +1,65 @@
 import Image from "next/image";
+import { Category, db } from "@/util/db";
+import Link from "next/link";
 
-export default function Footer() {
-  return (
-    <div className="grid grid-cols-4 gap-4 p-6 text-center items-center ">
-      <div>
-        <Image
-          src="/LogoTrans.png"
-          width={100}
-          height={40}
-          alt="Find if Faster Logo"
-          className="justify-items-center"
-        />
-        <br />
-      </div>
+export default async function Footer() {
+    const categories = await db().query<Category & { id: number }>(`SELECT cat.id, cat.name, (
+      SELECT count(*) 
+      FROM biz_business_category 
+      WHERE business = cat.id) AS total 
+      FROM biz_category cat 
+      ORDER BY total 
+      DESC LIMIT 4;`);
 
-      <div>
-        <h2 className="text-fuchsia-400">Categories:</h2>
-        <p>Category 1</p>
-        <p>Category 2</p>
-        <p>Category 3</p>
-        <p>Category 4</p>
-        <br />
-      </div>
-      <div>
-        <h2 className="text-fuchsia-400">Location:</h2>
-        <p>Location 1</p>
-        <p>Location 2</p>
-        <p>Location 3</p>
-        <p>Location 4</p>
-        <br />
-      </div>
-      <div>
-        <h2 className="text-fuchsia-400">Resources:</h2>
-        <p>About Us</p>
-        <p>Write a Review</p>
-        <p>Add a Business</p>
-        <p>Search for a company</p>
-        <br />
-      </div>
-    </div>
-  );
+    const locations = await db().query(`SELECT loc.id, loc.location_name, loc.country, (
+        SELECT count(*) 
+        FROM biz_businesses WHERE city = loc.id) as total 
+        FROM biz_locations loc 
+        ORDER BY total 
+        DESC LIMIT 4`);
+
+    return (
+        <div className="grid grid-cols-4 gap-4 p-6 text-center items-center ">
+            <div>
+                <Image
+                    src="/LogoTrans.png"
+                    width={100}
+                    height={40}
+                    alt="Find if Faster Logo"
+                    className="justify-items-center"
+                />
+                <br />
+            </div>
+
+            <div>
+                <h2 className="text-fuchsia-400">Popular Categories:</h2>
+                {categories.rows.map((category) => (
+                    <p key={category.id}>
+                        <Link href={`/search?category=${category.id}`}>{category.name}</Link>
+                    </p>
+                ))}
+                <br />
+            </div>
+            <div>
+                <h2 className="text-fuchsia-400">Popular Location:</h2>
+                {locations.rows.map((location) => (
+                    <p key={location.id}>
+                        <Link href={`/search?location=${location.id}`}>
+                            {location.location_name}, {location.country}
+                        </Link>
+                    </p>
+                ))}
+                <br />
+                <br />
+            </div>
+            <div>
+                <h2 className="text-fuchsia-400">Resources:</h2>
+                <p>About Us</p>
+                <p>Write a Review</p>
+                <p>Add a Business</p>
+                <p>Search for a company</p>
+                <br />
+            </div>
+        </div>
+    );
 }
