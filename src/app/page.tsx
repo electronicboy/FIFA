10079242ddaec264d111a2065ef244409c4de56c
Shane Freeder <theboyetronic@gmail.com>
2024-10-30 15:17:11 +0000

    Convert Map to React component, add POI magic

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
-import Maps from "@/components/layout/Maps";
+import Maps, {Poi, PoiMarkers} from "@/components/layout/Maps";
+import {Business, db} from "@/util/db";
 
-export default function Home() {
+export default async function Home() {
+    const businesses = await db().query<Business & {id: number}>( /* language=PostgreSQL */ "SELECT id, name, longitude, latitude FROM biz_businesses");
+    const poi: Poi[] = businesses.rows.map((business) => { return {key: business.id.toString(), location: {lat: business.latitude, lng: business.longitude}}})
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-            <Maps />
+            <Maps>
+                {poi && (
+                    <PoiMarkers pois={poi}/>
+                )}
+            </Maps >
             <main></main>
         </div>
     );
--- src/components/layout/Maps.tsx
+++ src/components/layout/Maps.tsx
@@ -1,51 +1,70 @@
 "use client";
 
-import React, { useEffect } from "react";
-import { Loader } from "@googlemaps/js-api-loader";
+import React, {ReactNode} from "react";
+//import { Loader } from "@googlemaps/js-api-loader";
+import {AdvancedMarker, APIProvider, Map, Pin} from '@vis.gl/react-google-maps';
 
-export default function Maps() {
-    const mapRef = React.useRef<HTMLDivElement>(null);
+export type Poi ={ key: string, location: google.maps.LatLngLiteral }
 
-    useEffect(() => {
-        const initMap = async () => {
-            const loader = new Loader({
-                apiKey: process.env.NEXT_PUBLIC_GOOOGLE_API_KEY as string,
-                version: "weekly",
-            });
+export const PoiMarkers = (props: {pois: Poi[]}) => {
+    return (
+        <>
+            {props.pois.map( (poi: Poi) => (
+                <AdvancedMarker
+                    key={poi.key}
+                    position={poi.location}>
+                    <Pin background={'#FBBC04'} glyphColor={'#000'} borderColor={'#000'} />
+                </AdvancedMarker>
+            ))}
+        </>
+    );
+};
 
-            const { Map } = await loader.importLibrary("maps");
 
-            //init a marker as google
-            //@ts-expect-error google told me to do this
-            const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+export default function Maps({children}:  {children?: ReactNode}) {
+    //const mapRef = React.useRef<HTMLDivElement>(null);
 
-            const position = {
-                lat: 43.642693,
-                lng: -79.3871189,
-            };
-
-            //map options
-            const mapOptions: google.maps.MapOptions = {
-                center: position,
-                zoom: 17,
-                mapId: "Finditfaster",
-            };
-
-            //SETUP THE MAP
-            const map = new Map(mapRef.current as HTMLDivElement, mapOptions);
-
-            //add a marker
-            new AdvancedMarkerElement({
-                map: map,
-                position: position,
-            });
-        };
-        initMap();
-    }, []);
+    // useEffect(() => {
+    //     const initMap = async () => {
+    //         const loader = new Loader({
+    //             apiKey: process.env.NEXT_PUBLIC_GOOOGLE_API_KEY as string,
+    //             version: "weekly",
+    //         });
+    //
+    //         const { Map } = await loader.importLibrary("maps");
+    //
+    //         //init a marker as google
+    //         //@ts-expect-error google told me to do this
+    //         const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+    //
+    //         const position = {
+    //             lat: 43.642693,
+    //             lng: -79.3871189,
+    //         };
+    //
+    //         //map options
+    //         const mapOptions: google.maps.MapOptions = {
+    //             center: position,
+    //             zoom: 17,
+    //             mapId: "Finditfaster",
+    //         };
+    //
+    //         //SETUP THE MAP
+    //         const map = new Map(mapRef.current as HTMLDivElement, mapOptions);
+    //
+    //         //add a marker
+    //         new AdvancedMarkerElement({
+    //             map: map,
+    //             position: position,
+    //         });
+    //     };
+    //     initMap();
+    // }, []);
 
     return (
         <>
             <div>
+
                 {/* <iframe
                     src="https://storage.googleapis.com/maps-solutions-uben8x1wu5/neighborhood-discovery/85j4/neighborhood-discovery.html"
                     width="1000"
@@ -54,7 +73,14 @@ export default function Maps() {
                     loading="eager"
                 ></iframe> */}
             </div>
-            <div style={{ height: "600px", width: "1000px" }} ref={mapRef} />
+            <div style={{height: "600px", width: "1000px"}}>
+                <APIProvider apiKey={process.env.NEXT_PUBLIC_GOOOGLE_API_KEY!}>
+                    <Map defaultZoom={13} defaultCenter={{lat: 53.40468, lng: -2.98034}} mapId={"HOME_MAP"}>
+                        {children}
+                    </Map>
+                </APIProvider>
+
+            </div>
         </>
     );
 }
